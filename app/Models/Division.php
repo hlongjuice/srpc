e0365827b7ab92238536dff334327831ad87302a
hlongjuice <isis.juicy@gmail.com>
2016-05-07 16:39:40 +0700

    gg

--- app/Models/Division.php
+++ app/Models/Division.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Division extends Model
 {
     protected $fillable=['parent_id','title'];
-    public function personnel(){
+    public function personnels(){
         return $this->belongsToMany('App\Models\Personnel','division_personnels');
     }
 }
